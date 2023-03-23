e975f318ba5feaa3795e662c28d762583f92f42d
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-23 10:37:50 +0600

    Salary_Increment in friend function

---
+++ friend.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Salary {
+  private:
+  int sal;
+  friend int increment (Salary s);
+};
+
+int increment (Salary s) {
+  s.sal = s.sal + 7000;
+  return s.sal;
+}
+
+int main() {
+  Salary s;
+  cout << "increment is : ";
+  cout << increment (s);
+
+  return 0;
+}
